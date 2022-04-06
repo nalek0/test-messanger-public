00eed1969fc733f5fea5438ded330a1071154302
nalek0 <belash.aleksandr777@gmail.com>
2022-04-06 18:34:41 +0300

    Raising exceptions in user api. Also made small refactoring

--- server/api_user.py
+++ server/api_user.py
@@ -1,66 +1,59 @@
-from flask import Blueprint, request, abort
+from flask import Blueprint, request
 from flask_login import login_required, current_user
-from werkzeug import exceptions
 
+from api_exceptions import APINotFound, APIBadRequest
 from database import User, db
 
 user_api = Blueprint("user_api", __name__,
                      url_prefix="/user")
 
 
-@user_api.route("/get_client_user", methods=["POST"])
+@user_api.route("/get_client_user", methods=["GET"])
 @login_required
 def get_client():
     return current_user.private_json()
 
 
-@user_api.route("/get_user", methods=["POST"])
+@user_api.route("/get_user", methods=["GET"])
 def get_user():
-    user_id = request.json.get("user_id")
-    if user_id is None:
-        abort(exceptions.BadRequest.code)
-
+    user_id = request.args["user_id"]
     user = User.query.get(user_id)
     if user is not None:
         return user.public_json()
     else:
-        return {}
+        raise APINotFound(f"User with given ({user_id}) is not found!")
 
 
 @user_api.route("/update_profile", methods=["POST"])
 @login_required
 def update_profile():
-    first_name = request.json.get("first_name")
-    last_name = request.json.get("last_name")
-    description = request.json.get("description") or ""
-    if first_name is None or last_name is None:
-        return abort(exceptions.BadRequest.code)
+    if request.json.get("first_name") is not None:
+        first_name = request.json.get("first_name").strip()
+        if first_name == "":
+            raise APIBadRequest("First name must not be blank")
+        current_user.first_name = first_name
+    if request.json.get("last_name") is not None:
+        last_name = request.json.get("last_name").strip()
+        if last_name == "":
+            raise APIBadRequest("Last name must not be blank")
+        current_user.last_name = last_name
+    if request.json.get("description") is not None:
+        description = request.json.get("description").strip()
+        current_user.description = description
 
-    first_name.strip()
-    last_name.strip()
-    description.strip()
-
-    if first_name == "" or last_name == "":
-        return abort(exceptions.BadRequest.code)
-
-    current_user.first_name = first_name
-    current_user.last_name = last_name
-    current_user.description = description
     db.session.commit()
     current_user.updated()
 
-    return {
-        "description": "OK"
-    }
+    return {"description": "OK"}
 
 
 @user_api.route("/add_friend", methods=["POST"])
 @login_required
 def add_friend():
-    user_id = request.json.get("user_id")
+    user_id = request.json["user_id"]
     user = User.query.get(user_id)
     if user is None:
-        abort(exceptions.BadRequest.code)
+        raise APINotFound(f"User with given ({user_id}) is not found!")
 
     if user not in current_user.friends:
         current_user.friends.append(user)
@@ -75,10 +68,10 @@ def add_friend():
 @user_api.route("/remove_friend", methods=["POST"])
 @login_required
 def remove_friend():
-    user_id = request.json.get("user_id")
+    user_id = request.json["user_id"]
     user = User.query.get(user_id)
     if user is None:
-        abort(exceptions.BadRequest.code)
+        raise APINotFound(f"User with given ({user_id}) is not found!")
 
     if user in current_user.friends:
         current_user.friends.remove(user)
